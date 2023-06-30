cbca36cf3afc0fa0ffaf89ec2b90b16b1cee3360
winnie <winnieyap20@gmail.com>
2023-06-30 23:15:31 +0800

    completed

---
+++ Geolocation/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Geolocation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+
+namespace Geolocation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Geolocation() {
+            GeoHelper geoHelper = new GeoHelper();
+            var result = await geoHelper.GetGeoInfo();
+            ViewData["GeoCode"] = result; // i got this shit then i wanna deserialize it 
+
+            var model = new GeolocationItem();
+            model = JsonSerializer.Deserialize<GeolocationItem>(result);
+            ViewData["GeolocationItem"] = model;
+
+
+            return View(model);  
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Geolocation/Models/GeoHelper.cs
@@ -0,0 +1,51 @@
+﻿using Geolocation.Models;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http.Json;
+
+namespace Geolocation.Models
+{
+    public class GeoHelper
+    {
+        private readonly HttpClient _httpClient;
+        private string apikey = "6e7bffd429766e92c13212450828c6a8";
+        public GeoHelper()
+        {
+            _httpClient = new HttpClient()
+            {
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
+        }
+
+        private async Task<string> GetIPAddress()
+        {
+            var ipAddress = await _httpClient.GetAsync($"http://ipinfo.io/ip");
+            if (ipAddress.IsSuccessStatusCode)
+            {
+                var json = await ipAddress.Content.ReadAsStringAsync();
+                return json.ToString();
+            }
+
+            return "";
+        }
+
+        public async Task<string> GetGeoInfo()
+        {
+            var ipAddress = await GetIPAddress();
+
+            var response = await _httpClient.GetAsync($"http://api.ipstack.com/" + ipAddress + "?access_key=" + apikey);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                return json;
+            }
+
+            return "";
+        }
+
+
+    }
+
+}
